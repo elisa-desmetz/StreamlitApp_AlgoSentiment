8a06fe5ddf2e7136b4bf113c33e569d65aa3ab27
elisa-desmetz <71330692+elisa-desmetz@users.noreply.github.com>
2020-12-05 01:45:24 +0100

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -1,13 +1,7 @@
 mkdir -p ~/.streamlit
 
-echo "\
-[general]\n\
-email = \"elisa.desmetz@gmail.com\"\n\
-" > ~/.streamlit/credentials.toml
-
-echo "\
-[server]\n\
-headless = true\n\
-port = $PORT\n\
-enableCORS = false\n\
+echo "[server]
+headless = true
+port = $PORT
+enableCORS = false
 " > ~/.streamlit/config.toml
